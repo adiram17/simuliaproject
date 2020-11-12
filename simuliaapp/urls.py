f98562290cca232d0f51399a50fad45c500c077e
adiramadhan <adi.ramadhan@sigma.co.id>
2020-11-12 22:02:47 +0700

    update urls.py

--- simuliaapp/urls.py
+++ simuliaapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views 
 
-
 urlpatterns = [
     path('', views.home, name='home'),
+    path('serelia_list', views.sereliaList, name='serelia_list'),
     path('serelia_create', views.sereliaCreate, name='serelia_create'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
